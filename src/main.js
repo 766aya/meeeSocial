faf65456762570bd2a1febdc59a11edd10ae7bd3
766aya <1494233259@qq.com>
2019-01-27 09:42:23 +0800

    update

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ import '@/style/common.scss'
 import banner from '@/components/banner'
 import crumbs from '@/components/crumbs'
 import channel from '@/components/channel'
+import description from '@/components/description'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
@@ -19,6 +20,7 @@ Vue.use(VueI18n)
 Vue.component('banner', banner)
 Vue.component('crumbs', crumbs)
 Vue.component('channel', channel)
+Vue.component('govDesc', description)
 
 const i18n = new VueI18n({
   locale: 'en', // 语言标识
