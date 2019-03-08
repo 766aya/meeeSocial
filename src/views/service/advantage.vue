257f42f3872408b291a07f00f61fef94a557b224
周庆强 <1494233259@qq.com>
2019-03-08 09:09:38 +0800

    update

--- src/views/service/advantage.vue
+++ src/views/service/advantage.vue
@@ -3,7 +3,7 @@
     <h1 class="gov-title">浩捷亚优势</h1>
     <div class="content">
       <slot></slot>
-      <a :href="learnMoreUrl" target="_blank" class="learn-more-btn">了解更多 ></a>
+      <!-- <a :href="learnMoreUrl" target="_blank" class="learn-more-btn">了解更多 ></a> -->
     </div>
   </div>
 </template>
