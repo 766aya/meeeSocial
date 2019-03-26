cb5e5d288636ef4efb0e6481a31261e7a8d5a10e
ChineseFish <565828928@qq.com>
2019-03-26 22:58:01 +0800

    footer

--- src/components/footer/index.vue
+++ src/components/footer/index.vue
@@ -1,6 +1,7 @@
 <template>
-  <div id="gov-footer">
-    <span style="text-align:center" href="http://www.miitbeian.gov.cn">苏ICP备19012764号-1</span>
+  <div class="gov-footer">
+  	<span style="margin:10px;">Copyright © 2018-2019 HJY Corporation, All Rights Reserved</span>
+    <a style="cursor:pointer;text-decoration:none;margin:10px;" href="http://www.miitbeian.gov.cn">苏ICP备19012764号-1</a>
   </div>
 </template>
 
@@ -12,8 +13,10 @@ export default {
 
 <style lang="scss" scoped>
 .gov-footer {
-	text-align: center;
-	height: 300px;
-	background-color: yellow;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	height: 100px;
 }
 </style>
